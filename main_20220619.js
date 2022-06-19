923b2d580111083bd70e52a0591b3779c5b902ca
honoka <h19981029@gmail.com>
2022-06-19 22:00:31 +0900

    📝関数の 練習

---
+++ main_20220619.js
@@ -0,0 +1,34 @@
+// function getName (firstName,lastName,age) {
+//     console.log (firstName,lastName,age)
+// }
+ 
+//  const getName = (firstName,lastName,age) =>{
+//     console.log (firstName,lastName,age)
+//  }
+
+
+// getName ("片山","ほのか",23)
+// getName ("片山","達文",26)
+
+
+
+// const getAge = (age1,age2) => {
+//       age1 + age2
+// }
+
+// let familyAge = getAge (23,26) 
+// console.log (familyAge)
+
+
+// const getTax = (price) =>{
+//     return (price * 0.1) + price
+// }
+// let tax = getTax (120)
+// console.log (tax)
+
+let half
+const halfPrice = (homePrice) => {
+    return(homePrice / 2)
+}
+half = halfPrice(56000)
+console.log (half)
