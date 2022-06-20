09d1d9c8d01bce33529890096944e062a7ffd52f
honoka <h19981029@gmail.com>
2022-06-20 22:33:50 +0900

    ⚡️ 練習２

---
+++ rensyu.js
@@ -0,0 +1,47 @@
+let lifePrice
+let lifePrice0
+
+// const sumPrice = (sweet, rice) => {
+//     lifePrice  = (sweet + rice) * 1.1
+//     if (lifePrice >= 500) {
+//         return lifePrice
+//     } else {
+//         return 0;
+//     }
+// }
+
+// lifePrice = sumPrice(200, 2500);
+// lifePrice0 = sumPrice(50, 300);
+// console.log (lifePrice)
+// console.log (lifePrice0)
+
+// let add
+
+// const sumPrice = (sweet, rice) => {
+//     lifePrice = (sweet + rice)
+// }
+// sumPrice(50, 300);
+// if (lifePrice >= 500) {
+//     add = (lifePrice)
+// } else {
+//     add = ("500円以上で購入できます")
+// }
+
+let hako
+let hako2
+let box
+
+const zidou = (price) => {
+    if(price = 100) {
+        hako = "cola"
+    }
+
+    box =  hako + "を購入しました"
+
+}
+ box = zidou(100)
+
+    console.log(hako)
+    console.log(box)
+
+
