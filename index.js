021050e914bfa3ef020796a2a3d24ddce07e7690
honoka <h19981029@gmail.com>
2022-06-26 12:39:33 +0900

    今までの練習を使ったやつ

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-async function callApi (){
-    const res =await fetch("https://pokeapi.co/api/v2/pokemon/pikachu/");
-    const users = await res.json();
-    console.log(users.name);
+//DOM
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+
+//関数
+async function getPkemon() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=5");
+    const pokemons = await res.json();
+    console.log(pokemons)
+    return pokemons;
 }
 
-callApi();
+//関数
+async function listPkemon(){
+    const pokemons = await getPkemon();
+    pokemons.results.forEach(pokemon => {
+        const list = document.createElement("li");
+        list.innerText = pokemon.name;
+        lists.appendChild(list);
+    });    
+}
+
+//イベント
+window.addEventListener("load",listPkemon)
+button.addEventListener("click",listPkemon);
+
